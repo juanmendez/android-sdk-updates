b851a9f2914ee4f8e237caf59a29f60436f23615
Juan Mendez <juan.mendez1@grainger.com>
2022-02-15 18:54:06 -0600

    permission util

--- app/src/main/java/com/example/sdkUpdates/MainActivity.kt
+++ app/src/main/java/com/example/sdkUpdates/MainActivity.kt
@@ -2,35 +2,15 @@ package com.example.sdkUpdates
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.sdkUpdates.databinding.ActivityMainBinding
 
 
 @SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    // Register the permissions callback, which handles the user's response to the
-    // system permissions dialog. Save the return value, an instance of
-    // ActivityResultLauncher. You can use either a val, as shown in this snippet,
-    // or a lateinit var in your onAttach() or onCreate() method.
-    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
-    private val preferences: SharedPreferences
-        get() = getPreferences(Context.MODE_PRIVATE)
-
-    private val sharedKey: String
-        get() = "isInitialized-${Manifest.permission.WRITE_EXTERNAL_STORAGE}"
+    private lateinit var permissionUtil: PermissionUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,69 +19,43 @@ class MainActivity : AppCompatActivity() {
             setContentView(root)
         }
 
-        // we check permission has been set for the first time
-        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            // storing if the user has made at least one time decision came from
-            // https://medium.com/@begalesagar/method-to-detect-if-user-has-selected-dont-ask-again-while-requesting-for-permission-921b95ded536
-            // there is not a way to track if the user has made a decision yet from Android
-            preferences.edit().putBoolean(sharedKey, true).apply()
-        }
+        permissionUtil = PermissionUtil(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 
     override fun onResume() {
         super.onResume()
-
-        val permissionValue = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        val isGranted = permissionValue == PackageManager.PERMISSION_GRANTED
-        updateDisplay(isGranted)
+        permissionUtil.refreshUpdate(::updateDisplay)
     }
 
-    private fun updateDisplay(isGranted: Boolean) {
-        // Explain to the user that the feature is unavailable because the
-        // features requires a permission that the user has denied. At the
-        // same time, respect the user's decision. Don't link to system
-        // settings in an effort to convince the user to change their
-        // decision.
-        when {
-            isGranted -> {
+    private fun updateDisplay(state: PermissionState) {
+        when (state) {
+            PermissionState.GRANTED -> {
                 binding.activityMainTextview.text = "Granted"
                 binding.activityMainTextview.setOnClickListener { }
             }
-            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+            PermissionState.TEMPORARILY_DENIED -> {
                 binding.activityMainTextview.text = "Temporarily denied"
 
                 binding.activityMainTextview.setOnClickListener {
-                    requestPermission()
+                    permissionUtil.request()
                 }
             }
-            preferences.getBoolean(sharedKey, false) -> {
+            PermissionState.PERMANENTLY_DENIED -> {
                 binding.activityMainTextview.text = "Permanently denied"
                 binding.activityMainTextview.setOnClickListener {
-                    launchAppPermissions()
+                    permissionUtil.request()
                 }
             }
             else -> {
                 binding.activityMainTextview.text = "First time"
                 binding.activityMainTextview.setOnClickListener { }
-                requestPermission()
+                permissionUtil.request()
             }
         }
     }
 
-    private fun requestPermission() {
-        requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    }
-
-    private fun launchAppPermissions() {
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            data = Uri.fromParts("package", packageName, null)
-        }
-        startActivity(intent)
-    }
-
     override fun onDestroy() {
         super.onDestroy()
-        requestPermissionLauncher.unregister()
+        permissionUtil.unregister()
     }
 }
---
+++ app/src/main/java/com/example/sdkUpdates/PermissionUtil.kt
@@ -0,0 +1,95 @@
+package com.example.sdkUpdates
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+enum class PermissionState {
+    NONE,
+    INITIAL,
+    GRANTED,
+    TEMPORARILY_DENIED,
+    PERMANENTLY_DENIED
+}
+
+class PermissionUtil(private val activity: AppCompatActivity, private val permissionType: String) {
+    companion object {
+        private const val PACKAGE = "package"
+    }
+
+    private var requestPermissionLauncher: ActivityResultLauncher<String>
+    private val preferences: SharedPreferences
+        get() = activity.getPreferences(Context.MODE_PRIVATE)
+
+    private var _currentState = PermissionState.NONE
+
+    @Suppress("unused")
+    val currentState: PermissionState
+        get() = _currentState
+
+    private var permissionListener: (PermissionState) -> Unit = {}
+
+
+    private val sharedKey: String
+        get() = "isInitialized-$permissionType"
+
+    init {
+        requestPermissionLauncher = activity.registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            preferences.edit().putBoolean(sharedKey, true).apply()
+            updateDisplay(isGranted)
+        }
+    }
+
+    fun refreshUpdate(listener: (PermissionState) -> Unit) {
+        val permissionValue = ContextCompat.checkSelfPermission(activity, permissionType)
+        val isGranted = permissionValue == PackageManager.PERMISSION_GRANTED
+        permissionListener = listener
+        updateDisplay(isGranted)
+    }
+
+    private fun updateDisplay(isGranted: Boolean) {
+        val lastState = _currentState
+
+        _currentState = when {
+            isGranted -> {
+                PermissionState.GRANTED
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(activity, permissionType) -> {
+                PermissionState.TEMPORARILY_DENIED
+            }
+            preferences.getBoolean(sharedKey, false) -> {
+                PermissionState.PERMANENTLY_DENIED
+            }
+            else -> {
+                PermissionState.INITIAL
+            }
+        }
+
+        // prevent to update if both init and refreshUpdate call sequentially
+        if (lastState != _currentState) {
+            permissionListener(_currentState)
+        }
+    }
+
+    fun request() {
+        if (_currentState == PermissionState.PERMANENTLY_DENIED) {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                data = Uri.fromParts(PACKAGE, activity.packageName, null)
+            }
+            activity.startActivity(intent)
+        } else {
+            requestPermissionLauncher.launch(permissionType)
+        }
+    }
+
+    fun unregister() = requestPermissionLauncher.unregister()
+}
